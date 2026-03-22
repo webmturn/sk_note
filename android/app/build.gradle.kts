95b77a4e7195f5e33ca886542ced6130599bf2d2
Tian yi <webmturn@gmail.com>
2026-03-23 00:00:31 +0800

    bump: versionCode=4, versionName=1.2.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sknote.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         // TODO: 部署后替换为你的 Cloudflare Workers URL
         buildConfigField("String", "API_BASE_URL", "\"https://api.wsqh.cn\"")
